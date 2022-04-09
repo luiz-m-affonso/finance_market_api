0bb33e46e59b82c270a3f3ed13e1ab6016d37e77
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-04-08 23:45:41 -0300

    RSpec configuration

--- web-app/config/application.rb
+++ web-app/config/application.rb
@@ -23,6 +23,7 @@ module FinanceMarketApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.generators.test_framework = :rspec
 
     # Configuration for the application, engines, and railties goes here.
     #
