0c818f5f434cf688da9380a48a0f1c6aab63d0c5
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-04-09 00:53:17 -0300

    Add validation for account

--- web-app/app/models/finances/account.rb
+++ web-app/app/models/finances/account.rb
@@ -1,4 +1,8 @@
 class Finances::Account < ApplicationRecord
   belongs_to :customer
   has_many :transactions
+
+  validates_presence_of :balance
+  validates_presence_of :type
+  validates_presence_of :customer_id
 end
