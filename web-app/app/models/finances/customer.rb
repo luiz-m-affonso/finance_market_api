74ecf00a1fd186761846b39ed9a48e97f6e66c80
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-04-09 12:41:30 -0300

    refactor: Customer and specs

--- web-app/app/models/finances/customer.rb
+++ web-app/app/models/finances/customer.rb
@@ -2,13 +2,5 @@ class Customer < ApplicationRecord
   has_many :accounts
   has_many :transactions, through: :accounts
 
-  validates_presence_of :email
-  validates_presence_of :account_id
-  validates_presence_of :first_name
-  validates_presence_of :last_name
-  validates_presence_of :address
-  validates_presence_of :city
-  validates_presence_of :state
-  validates_presence_of :zip
-  validates_presence_of :phone
+  validates_presence_of :email, :first_name, :last_name, :address, :city, :state, :country, :zip, :phone
 end
