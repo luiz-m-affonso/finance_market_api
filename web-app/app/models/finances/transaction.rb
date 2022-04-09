5cdff342e5979baf80ae071ee5cf4a5535f05ddf
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-04-09 12:40:54 -0300

    refactor: Transaction and specs

--- web-app/app/models/finances/transaction.rb
+++ web-app/app/models/finances/transaction.rb
@@ -2,8 +2,5 @@ class Transaction < ApplicationRecord
   belongs_to :customer
   belongs_to :account
 
-  validates_presence_of :amount
-  validates_presence_of :date
-  validates_presence_of :type
-  validates_presence_of :description
+  validates_presence_of :account_id, :customer_id, :amount, :date, :type, :description
 end
---
+++ web-app/spec/models/finances/transaction_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Transaction, type: :model do
+  let(:subject) { described_class.new(amount: '100', date: Date.today, type: 'Transference', description: 'Pay the bills')}
+
+  describe 'relations and validations' do
+    it { is_expected.to belongs_to(:customer) }
+    it { is_expected.to belongs_to(:account) }
+    it { is_expected.to validate_presence_of(:amount) }
+    it { is_expected.to validate_presence_of(:date) }
+    it { is_expected.to validate_presence_of(:type) }
+    it { is_expected.to validate_presence_of(:description)}
+
+
+    context 'when Transaction has valid attributes' do
+      it { is_expected.to be_valid }
+    end
+
+    context 'when Transaction has invalid attributes' do
+      let(:transaction) { described_class.new(amount: nil, date: Date.today, type: nil, description: nil) }
+      it { expect(transaction).not_to be_valid }
+    end
+  end
+end
