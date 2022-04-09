92b33eb3544e259b7b51faf1c4ce6b753cca5fa0
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-04-09 12:41:46 -0300

    refactor: Account and specs

--- web-app/app/models/finances/account.rb
+++ web-app/app/models/finances/account.rb
@@ -2,7 +2,5 @@ class Finances::Account < ApplicationRecord
   belongs_to :customer
   has_many :transactions
 
-  validates_presence_of :balance
-  validates_presence_of :type
-  validates_presence_of :customer_id
+  validates_presence_of :balance, :type, :customer_id
 end
--- web-app/spec/models/finances/account_spec.rb
+++ web-app/spec/models/finances/account_spec.rb
@@ -4,32 +4,19 @@ RSpec.describe Account, type: :model do
   let(:subject) { described_class.new(customer_id: 1, balance: '100.00', type: 'Business Account') }
 
   describe 'relations and validations' do
+    it { is_expected.to belongs_to(:customer) }
+    it { is_expected.to have_many(:transactions) }
     it { is_expected.to validate_presence_of(:customer_id) }
     it { is_expected.to validate_presence_of(:balance) }
     it { is_expected.to validate_presence_of(:type) }
 
-    it 'belongs to a user' do
-      is_expected.to belong_to(:user)
-    end
-
-    it 'has many transactions' do
-      is_expected.to have_many(:transactions)
+    context 'when Account has valid attributes' do
+      it { is_expected.to be_valid }
     end
 
     context 'when Account has invalid attributes' do
       let(:account) { described_class.new(customer_id: nil, balance: nil, type: nil) }
-
-      it 'is not valid without a name' do
-        is_expected.not_to be_valid
-      end
-
-      it 'is not valid without a balance' do
-        is_expected.not_to be_valid
-      end
-
-      it 'is not valid without a user' do
-        is_expected.not_to be_valid
-      end
+      it { expect(account).not_to be_valid }
     end
   end
 end
