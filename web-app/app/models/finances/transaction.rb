29a5b72806128d0c6bbc0eeb102b73813c3e4e2c
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-04-09 00:53:39 -0300

    Add validation for transaction

--- web-app/app/models/finances/transaction.rb
+++ web-app/app/models/finances/transaction.rb
@@ -1,4 +1,9 @@
 class Transaction < ApplicationRecord
   belongs_to :customer
   belongs_to :account
+
+  validates_presence_of :amount
+  validates_presence_of :date
+  validates_presence_of :type
+  validates_presence_of :description
 end
