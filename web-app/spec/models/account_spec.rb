5ba8d1d64a2c03b9e50fb4c5381cd8fafcd2bf89
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-04-09 00:54:20 -0300

    Create Account model spec

--- web-app/spec/models/account_spec.rb
+++ web-app/spec/models/account_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:subject) { described_class.new(customer_id: 1, balance: '100.00', type: 'Business Account') }
+
+  describe 'relations and validations' do
+    it { is_expected.to validate_presence_of(:customer_id) }
+    it { is_expected.to validate_presence_of(:balance) }
+    it { is_expected.to validate_presence_of(:type) }
+
+    it 'belongs to a user' do
+      is_expected.to belong_to(:user)
+    end
+
+    it 'has many transactions' do
+      is_expected.to have_many(:transactions)
+    end
+
+    context 'when Account has invalid attributes' do
+      let(:account) { described_class.new(customer_id: nil, balance: nil, type: nil) }
+
+      it 'is not valid without a name' do
+        is_expected.not_to be_valid
+      end
+
+      it 'is not valid without a balance' do
+        is_expected.not_to be_valid
+      end
+
+      it 'is not valid without a user' do
+        is_expected.not_to be_valid
+      end
+    end
+  end
 end
+
