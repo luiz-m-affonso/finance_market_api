113fb541488b8be6910e0f7fe3df000b56437dc9
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-05-15 19:15:06 -0300

    Remove customer from module finances

--- app/models/finances/customer.rb
+++
@@ -1,6 +0,0 @@
-class Customer < ApplicationRecord
-  has_many :accounts
-  has_many :transactions, through: :accounts
-
-  validates_presence_of :email, :first_name, :last_name, :address, :city, :state, :country, :zip, :phone
-end
