a30a5c5739e6f07e757e757c4bd636428201d48d
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-05-15 19:18:20 -0300

    Update factory customer

--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :customer do
-    password_digest { Faker::Password.digest }
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    email { Faker::Email.email }
-    phone { Faker::Phone.phone }
-    city { Faker::City.city }
-    state { Faker::State.state }
-    zip { Faker::Zip.zip }
-    country { Faker::Country.country }
+    id { 1 }
+    password_digest { "MyString" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+    email { "MyString" }
+    phone { "MyString" }
+    city { "MyString" }
+    state { "MyString" }
+    zip { "MyString" }
   end
 end
