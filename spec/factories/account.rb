b50b76947d296d0c4654e2250f19789d648b515a
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-05-15 19:18:14 -0300

    Update factory account

--- spec/factories/account.rb
+++ spec/factories/account.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :account do
-    balance { Faker::Number.decimal(2) }
-    type { %w[checking savings money_market_deposit certificate_of_deposit].sample }
+    id { 2 }
+    balance { 100.00}
+    account_type { %w[checking savings money_market_deposit certificate_of_deposit].sample }
+    customer_id { 1 }
   end
 end
