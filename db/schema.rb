114305e7352dd16414276bfc65c8de4a97980f7a
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-05-15 19:15:51 -0300

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_09_031537) do
+ActiveRecord::Schema.define(version: 2022_05_15_220508) do
 
   create_table "accounts", force: :cascade do |t|
     t.string "balance"
-    t.string "type"
-    t.integer "customer_id", null: false
+    t.string "account_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "customer_id"
     t.index ["customer_id"], name: "index_accounts_on_customer_id"
   end
 
   create_table "customers", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
-    t.integer "account_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
@@ -34,24 +33,19 @@ ActiveRecord::Schema.define(version: 2022_04_09_031537) do
     t.string "state"
     t.string "zip"
     t.string "phone"
-    t.index ["account_id"], name: "index_customers_on_account_id"
-    t.index ["email"], name: "index_customers_on_email", unique: true
   end
 
   create_table "transactions", force: :cascade do |t|
     t.string "amount"
     t.datetime "date"
-    t.string "type"
-    t.integer "customer_id", null: false
-    t.integer "account_id", null: false
+    t.string "transaction_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "description"
+    t.integer "customer_id"
+    t.integer "account_id"
     t.index ["account_id"], name: "index_transactions_on_account_id"
     t.index ["customer_id"], name: "index_transactions_on_customer_id"
   end
 
-  add_foreign_key "accounts", "customers"
-  add_foreign_key "customers", "accounts"
-  add_foreign_key "transactions", "accounts"
-  add_foreign_key "transactions", "customers"
 end
