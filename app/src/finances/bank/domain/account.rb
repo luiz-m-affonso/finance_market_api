18ef4350f6ea04aa1d73f466c0e0de028d0b470a
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-05-15 21:49:31 -0300

    Update domain account

--- app/src/finances/bank/domain/account.rb
+++ app/src/finances/bank/domain/account.rb
@@ -1,20 +1,24 @@
-module Finances
-  module Bank
-    class Account
-      attr_reader :id, :customer_id, :balance
-      def initialize(id:, customer_id:, balance:)
-        @id = id
-        @customer_id = customer_id
-        @balance = balance
-      end
+  module Finances
+    module Bank
+      module Domain
+        class Account
+          attr_reader :id, :customer_id, :balance, :account_type
+          def initialize(id:, customer_id:, balance:, account_type:)
+            @id = id
+            @customer_id = customer_id
+            @balance = balance
+            @account_type = account_type
+          end
 
-      def to_hash
-        {
-          id: @id,
-          customer_id: @customer_id,
-          balance: @balance
-        }
+          def to_hash
+            {
+              id: @id,
+              customer_id: @customer_id,
+              balance: @balance,
+              account_type: @account_type
+            }
+          end
+        end
       end
     end
   end
-end
