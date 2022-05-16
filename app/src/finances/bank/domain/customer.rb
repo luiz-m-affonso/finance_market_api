729e27dd36e9d07f5a2e0a5b25e905f0b4fbe74e
luiz-m-affonso <luizbuchhalter@outlook.com>
2022-05-15 21:59:15 -0300

    Update domain with id

--- app/src/finances/bank/domain/customer.rb
+++ app/src/finances/bank/domain/customer.rb
@@ -1,32 +1,34 @@
 module Finances
   module Bank
-    class Customer
-      attr_reader :email, :first_name, :last_name, :address, :city, :state, :zip, :phone
+    module Domain
+      class Customer
+        attr_reader :id, :email, :first_name, :last_name, :address, :city, :state, :zip, :phone
 
-      def initialize(id:, email:, first_name:, last_name:, address:, city:, state:, zip:, phone:)
-        @id = id
-        @email = email
-        @first_name = first_name
-        @last_name = last_name
-        @address = address
-        @city = city
-        @state = state
-        @zip = zip
-        @phone = phone
-      end
+        def initialize(id:, email:, first_name:, last_name:, address:, city:, state:, zip:, phone:)
+          @id = id
+          @email = email
+          @first_name = first_name
+          @last_name = last_name
+          @address = address
+          @city = city
+          @state = state
+          @zip = zip
+          @phone = phone
+        end
 
-      def to_hash
-        {
-          id: @id,
-          email: @email,
-          first_name: @first_name,
-          last_name: @last_name,
-          address: @address,
-          city: @city,
-          state: @state,
-          zip: @zip,
-          phone: @phone
-        }
+        def to_hash
+          {
+            id: @id,
+            email: @email,
+            first_name: @first_name,
+            last_name: @last_name,
+            address: @address,
+            city: @city,
+            state: @state,
+            zip: @zip,
+            phone: @phone
+          }
+        end
       end
     end
   end
